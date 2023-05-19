700a9030929b4746fd9dd30ba484dc7c43737f88
Lucas Anderson de Souza Marques <lucasandersonmarques@gmail.com>
2023-05-18 21:56:23 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplicacao(a: number, b: number) {
+    return a * b;
+}
+
+function dizOi(nome: string) {
+    return `Olá ${nome}`;
+}
+
+multiplicacao(5, 2);
+dizOi("Lucas")
